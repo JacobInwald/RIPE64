1f881f93baf702115bd3810efa4e3d2086dbc541
JacobInwald <Jacob.Inwald@Hotmail.com>
2025-02-26 15:24:17 +0000

    feat: updating RIPE with argparse

--- Makefile
+++ Makefile
@@ -1,15 +1,16 @@
 GCC=gcc
 CLG=clang
+
 # Disable stack protection, make the stack executable and add debug info
-CFLAGS=-fno-stack-protector -z execstack -g
+CFLAGS=-fno-stack-protector -z execstack -g ${HARDEN_FLAGS}
 
 all: build/gcc_attack_gen build/clang_attack_gen
 
-build/gcc_attack_gen: ./source/attack_gen.c
-	${GCC} ${CFLAGS} ./source/attack_gen.c -o ./build/gcc_attack_gen
+build/gcc_attack_gen: ./src/attack_gen.c
+	${GCC} ${CFLAGS} ./src/attack_gen.c -o ./build/gcc_attack_gen
 
-build/clang_attack_gen: ./source/attack_gen.c
-	${CLG} ${CFLAGS} ./source/attack_gen.c -o ./build/clang_attack_gen
+build/clang_attack_gen: ./src/attack_gen.c
+	${CLG} ${CFLAGS} ./src/attack_gen.c -o ./build/clang_attack_gen
 
 clean:
 	rm ./build/*
---
+++ flag_tester.py
@@ -0,0 +1,56 @@
+#!/bin/python
+import os, argparse
+
+# Defaults
+compiler = 'gcc'
+
+stkcan = '-fstack-protector-strong'
+dforti = '-DFORTIFY_SOURCE=3 -O2'
+stkcla = '-fstack-clash-protection'
+fcfpro = '-fcf-protection=full'
+mshstk = '-mshstk'
+
+parser = argparse.ArgumentParser(
+                    prog='RIPE64 - Flag Tester',
+                    description='''A python script to test specific combinations of flags with the RIPE64 tester.''', 
+                    epilog='')
+parser.add_argument('--stkcan',
+                    action='store_true',
+                    help='Adds stack canaries to test list')
+parser.add_argument('--stkcla',
+                    action='store_true',
+                    help='Adds stack clash protection to test list')
+parser.add_argument('--dforti',
+                    action='store_true',
+                    help='Adds DFORTIFY_SOURCE to test list')
+parser.add_argument('--fcfpro',
+                    action='store_true',
+                    help='Adds Intels CET to test list')
+parser.add_argument('--mshstk',
+                    action='store_true',
+                    help='Adds Intels Shadow Stack to test list')
+
+
+args = parser.parse_args()
+
+HARDEN_FLAGS = ''
+HARDEN_FLAGS += stkcan if args.stkcan else ''
+HARDEN_FLAGS += ' ' + stkcla if args.stkcla else ''
+HARDEN_FLAGS += ' ' + dforti if args.dforti else ''
+HARDEN_FLAGS += ' ' + fcfpro if args.fcfpro else ''
+HARDEN_FLAGS += ' ' + mshstk if args.mshstk else ''
+os.environ['HARDEN_FLAGS'] = HARDEN_FLAGS
+
+fp = 'out-'
+fp += 'stkcan-' if args.stkcan else ''
+fp += 'stkcla-' if args.stkcla else ''
+fp += 'dforti-' if args.dforti else ''
+fp += 'fcfpro-' if args.fcfpro else ''
+fp += 'mshstk-' if args.mshstk else ''
+fp = fp[:-1]
+
+os.makedirs('data', exist_ok=True)
+os.makedirs('build', exist_ok=True)
+os.system('make clean')
+os.system('make build/gcc_attack_gen')
+os.system(f'./ripe_tester.py -t both -n 1 -f latex > data/{fp}')
