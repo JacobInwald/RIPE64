a0eebf46af83c30401cbfadff00b2ba320a7e41d
JacobInwald <Jacob.Inwald@Hotmail.com>
2025-03-19 17:46:00 +0000

    feat: adding back files

--- collect.sh
+++ collect.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-./flag_tester.py 
-./flag_tester.py -s
-./flag_tester.py -c
-./flag_tester.py -d
-./flag_tester.py -m --enable_hardware_cet
+# ./flag_tester.py 
+# ./flag_tester.py -s
+# ./flag_tester.py -d
+./flag_tester.py -m --enable_hardware_cet 
+./flag_tester.py -sd
+./flag_tester.py -sdm --enable_hardware_cet
+
 ./flag_tester.py -f
+./flag_tester.py -sdf
